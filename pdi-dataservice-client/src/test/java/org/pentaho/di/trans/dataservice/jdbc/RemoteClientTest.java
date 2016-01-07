bcbca5651aeeed2e65937b50cc3df05f3abd7ee8
Benjamin Morrise <bmorrise@pentaho.com>
2016-01-07 16:17:28 -0500

    [PDI-12569] Moved sql query to post body

--- pdi-dataservice-client/src/test/java/org/pentaho/di/trans/dataservice/jdbc/RemoteClientTest.java
+++ pdi-dataservice-client/src/test/java/org/pentaho/di/trans/dataservice/jdbc/RemoteClientTest.java
@@ -31,6 +31,7 @@ import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.lang.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -50,6 +51,7 @@ import static org.hamcrest.Matchers.emptyOrNullString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
@@ -92,7 +94,7 @@ public class RemoteClientTest {
     int maxRows = 200;
 
     when( connection.getDebugTransFilename() ).thenReturn( debugTrans );
-    when( connection.getParameters() ).thenReturn( ImmutableMap.of("PARAMETER_ECHO", "hello world") );
+    when( connection.getParameters() ).thenReturn( ImmutableMap.of( "PARAMETER_ECHO", "hello world" ) );
     when( connection.isDebuggingRemoteLog() ).thenReturn( true );
 
     when( httpClient.executeMethod( isA( PostMethod.class ) ) ).thenReturn( 200 );
@@ -110,10 +112,42 @@ public class RemoteClientTest {
     assertThat( httpMethod.getURI().toString(), equalTo( "http://localhost:9080/pentaho-di/kettle/sql" ) );
     assertThat( httpMethod.getRequestHeader( "SQL" ).getValue(), equalTo( "SELECT * FROM myService WHERE id = 3" ) );
     assertThat( httpMethod.getRequestHeader( "MaxRows" ).getValue(), equalTo( "200" ) );
+    assertThat( httpMethod.getParameter( "SQL" ).getValue(), equalTo( "SELECT * FROM myService WHERE id = 3" ) );
+    assertThat( httpMethod.getParameter( "MaxRows" ).getValue(), equalTo( "200" ) );
 
     assertThat( httpMethod.getParameter( "debugtrans" ).getValue(), equalTo( debugTrans ) );
     assertThat( httpMethod.getParameter( "debuglog" ).getValue(), equalTo( "true" ) );
     assertThat( httpMethod.getParameter( "PARAMETER_ECHO" ).getValue(), equalTo( "hello world" ) );
+
+    assertThat( queryResponse.readUTF(), equalTo( "Query Response" ) );
+  }
+
+  @Test
+  public void testLargeQuery() throws Exception {
+    String sql = "SELECT * FROM myService\nWHERE id = 3 /" + StringUtils.repeat( "*", 8000 ) + "/";
+
+    when( connection.getDebugTransFilename() ).thenReturn( null );
+    when( connection.getParameters() ).thenReturn( ImmutableMap.<String, String>of() );
+    when( connection.isDebuggingRemoteLog() ).thenReturn( true );
+
+    when( httpClient.executeMethod( isA( PostMethod.class ) ) ).thenReturn( 200 );
+
+    MockDataInput mockDataInput = new MockDataInput();
+    mockDataInput.writeUTF( "Query Response" );
+
+    when( execMethod.getResponseBodyAsStream() ).thenReturn( mockDataInput.toDataInputStream() );
+
+    DataInputStream queryResponse = remoteClient.query( sql, 200 );
+
+    verify( httpClient ).executeMethod( httpMethodCaptor.capture() );
+    PostMethod httpMethod = (PostMethod) httpMethodCaptor.getValue();
+
+    assertThat( httpMethod.getURI().toString(), equalTo( "http://localhost:9080/pentaho-di/kettle/sql" ) );
+    assertThat( httpMethod.getRequestHeader( "SQL" ), is( nullValue() ) );
+    assertThat( httpMethod.getRequestHeader( "MaxRows" ), is( nullValue() ) );
+    assertThat( httpMethod.getParameter( "SQL" ).getValue(),
+        equalTo( "SELECT * FROM myService WHERE id = 3 /" + StringUtils.repeat( "*", 8000 ) + "/" ) );
+    assertThat( httpMethod.getParameter( "MaxRows" ).getValue(), equalTo( "200" ) );
 
     assertThat( queryResponse.readUTF(), equalTo( "Query Response" ) );
   }
