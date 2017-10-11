65e78751f7ec1cd16f44405402ca7b2774f8ac65
smaring <smaring@pentaho.com>
2017-10-11 08:17:51 -0400

    [ENGOPS-3544] change Pentaho to Hitachi Vantara and update copyright date

--- pdi-dataservice-client/src/main/java/org/pentaho/di/trans/dataservice/client/DataServiceConnectionInformation.java
+++ pdi-dataservice-client/src/main/java/org/pentaho/di/trans/dataservice/client/DataServiceConnectionInformation.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Community Edition Project: data-refinery-pdi-plugin
  *
- * Copyright (C) 2002-2017 by Pentaho : http://www.pentaho.com
+ * Copyright (C) 2002-2017 by Hitachi Vantara : http://www.pentaho.com
  *
  * *******************************************************************************
  *
