ef127f0a63bbd7417f37d408afc31c91edafde78
Denis Protopopov <Denis_Protopopov@epam.com>
2017-12-25 11:14:34 +0400

    [SP-4080][PDI-16671]-"Something Went Wrong" error message shows up when trying to Inspect Data which has a field name with Double Quotes.
    - fixed
    - additional fix. for pivot table
    - additional fix. when we use the filed name with double quotes as measure

--- pdi-dataservice-client/src/main/java/org/pentaho/di/core/jdbc/ThinUtil.java
+++ pdi-dataservice-client/src/main/java/org/pentaho/di/core/jdbc/ThinUtil.java
@@ -386,7 +386,7 @@ public class ThinUtil {
         } else {
           // Make sure to take escaping into account for single quotes
           //
-          index = findNext( sql, nextChar, index, skipChar == '\'' );
+          index = findNext( sql, nextChar, index, skipChar == '\'' || skipChar == '\"' );
         }
         if ( index >= sql.length() ) {
           break;
@@ -655,4 +655,15 @@ public class ThinUtil {
     }
     return Optional.ofNullable( valueMeta );
   }
+
+  /**
+  * Remove first and last quote if string contains double quote
+  * */
+  public static String unQuote( String str ) {
+    if ( str.contains( "\"\"" ) &&  str.length() > 0 && str.startsWith( "\"" ) && str.endsWith( "\"" ) ) {
+      return str.substring( 1, str.length() - 1 );
+    }
+    return str;
+  }
+
 }
